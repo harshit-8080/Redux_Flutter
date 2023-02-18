61ae1bef9662f66e81770c9fd79e99845c11730a
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2023-02-18 20:40:35 +0530

    Project Setup for flutter

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Redux App',
+        theme: ThemeData(primarySwatch: Colors.green),
+        home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Redux Application"),
+      ),
+    );
+  }
+}
