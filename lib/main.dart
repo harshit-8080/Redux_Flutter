bccc6c2935cda952ca07e1da2bb652db8e8ee151
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2023-02-19 23:57:37 +0530

    redux and flutter-redux

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
         title: 'Redux App',
         theme: ThemeData(primarySwatch: Colors.green),
         home: MyHomePage());
@@ -26,3 +27,9 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
+
